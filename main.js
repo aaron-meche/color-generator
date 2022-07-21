f183600298c596102bff88956563359f65039d2e
aaronMeche <70254600+aaron-meche@users.noreply.github.com>
2022-07-20 21:54:28 -0500

    stable 0 alpha

--- main.js
+++ main.js
@@ -21,20 +21,3 @@ function randomizeColor() {
         document.getElementById('colorLabel' + i).innerHTML = color;
     }
 }
-
-function copyColor(self) {
-
-    console.log('copying text');
-    /* Get the text field */
-    var copyText = self;
-
-    /* Select the text field */
-    copyText.select(); 
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-    /* Copy the text inside the text field */
-    navigator.clipboard.writeText(copyText.value);
-
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
-}
